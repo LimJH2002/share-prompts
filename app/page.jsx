4fc3e42acbcd46a7507be17232503542eef561e0
L1MJH <lolflgc@icloud.com>
2023-06-06 13:25:34 +0800

    Added Components

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,24 @@
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+          Discover & Share
+          <br className="max-md:hidden" />
+          <span className="orange_gradient text-center">
+            {" "}
+            AI-Powered Prompts
+          </span>
+        </h1>
+        <p className="desc text-center">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod tenetur
+          voluptas ipsum laboriosam assumenda dolorem explicabo quam
+          perspiciatis, velit excepturi tempora qui officiis deleniti eaque
+          blanditiis dignissimos repudiandae libero quas!
+        </p>
+      </section>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
