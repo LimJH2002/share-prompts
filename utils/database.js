8b71936e899a1ce4417db8f36f1b1a8f7cfd448b
L1MJH <lolflgc@icloud.com>
2023-06-06 15:59:19 +0800

    Sign in with Google up and running

--- utils/database.js
+++ utils/database.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
-let isConnected = false; //track the connection
+let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
+
   if (isConnected) {
     console.log("MongoDB is already connected");
     return;
@@ -15,9 +16,11 @@ export const connectToDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     isConnected = true;
-    console.log('MongoDB connected')
+
+    console.log("MongoDB connected");
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
